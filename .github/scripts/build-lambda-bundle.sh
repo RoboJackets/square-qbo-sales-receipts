10751cc39d4ce48051bc4114e398854b06ed1a1a
Kristaps Berzinch <kristapsberzinch@gmail.com>
2026-08-23 11:07:03 -0400

    Build bundle inside Lambda container for compatibility

---
+++ .github/scripts/build-lambda-bundle.sh
@@ -0,0 +1,31 @@
+set -xeuo pipefail
+
+dnf -y install gcc gcc-c++ make curl libffi-devel zip || microdnf -y install gcc gcc-c++ make curl libffi-devel zip
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable
+source "$HOME/.cargo/env"
+
+python3.15 -m pip install --upgrade pip setuptools wheel
+
+mkdir -p _bundle/lib/python3.15/site-packages
+
+python3.15 -m pip install cryptography==50.0.0 \
+  --platform manylinux_2_34_aarch64 \
+  --python-version 315 \
+  --implementation cp \
+  --abi abi3 \
+  --only-binary cryptography \
+  --no-deps \
+  --target _bundle/lib/python3.15/site-packages
+
+python3.15 -c 'import re; open("requirements-no-crypto.txt","w").write(re.sub("^cryptography==.*$","",open("requirements.txt").read(),flags=re.MULTILINE))'
+
+python3.15 -m pip install -r requirements-no-crypto.txt \
+  --platform manylinux_2_34_aarch64 \
+  --python-version 315 \
+  --no-deps \
+  --target _bundle/lib/python3.15/site-packages
+
+cd _bundle/lib/python3.15/site-packages
+rm -rf boto*
+zip -r ../../../../_bundle.zip .
